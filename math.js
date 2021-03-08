c06c3acf98b30a015524ced429d3310d5bb9c5bc
jui29 <jui29>
2021-03-08 09:19:57 +0600

    first commit

---
+++ math.js
@@ -0,0 +1,532 @@
+//var fullname="shah juhi"
+//fullname=fullname.toLocaleUpperCase();
+
+//console.log(fullname);
+
+
+//var Subject="Social Media Marketing";
+//Subject=Subject.toLocaleUpperCase();
+
+//console.log(Subject);
+
+
+
+//var fullname="shah juhina begum";
+//fullname=fullname.toLowerCase();
+
+//console.log(fullname);
+
+
+
+//var dummyText="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
+//var checkDummy= dummyText.indexOf("simply");
+
+//console.log(checkDummy);
+
+
+//var dummyText="Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+
+//var checkDummy= dummyText.split("");
+
+//console.log(checkDummy);
+
+
+
+//var FullName="shah juhi";
+//var checkName=FullName.split("  ");
+//console.log(checkName);
+
+
+
+//var Subject1="SMM";
+//var Subject3="Music";
+
+//var total=Subject1 + " " + Subject3;
+
+//console.log(total);
+
+
+
+//var price1=14;
+//var price2=2;
+
+//var total= price1 * price2;
+
+//console.log(total);
+
+
+
+//var cost1=40;
+//var cost2=20;
+
+//var total =cost1 % cost2;
+
+//console.log(total);
+
+
+//var cost1=40;
+//var cost2=20;
+
+//var total =cost1 / cost2;
+
+//console.log(total);
+
+
+
+//var cost1=40;
+//var cost2=20;
+
+//var total =cost1 + 3;
+
+//console.log(total);
+
+
+
+//var cost1=40;
+//var cost2=20;
+
+//cost2++;
+
+//console.log(cost2);
+
+
+var cost1=40;
+var cost2=20;
+
+cost1--;
+
+console.log(cost1);
+
+
+//var cost=400;
+
+//var price1=cost;
+
+//console.log(price1);
+
+//var cost1=50;
+//var cost2=50;
+
+//var Ans=cost1==cost2;
+
+//console.log(Ans);
+
+
+//var cost1=50;
+//var cost2=56;
+
+//var Ans=cost1==cost2;
+
+//console.log(Ans);
+
+
+//var cost1=50;
+//var cost2="50";
+
+//var Ans=cost1===cost2;
+
+//console.log(Ans);
+
+
+//var cost1="50";
+//var cost2="50";
+
+//var Ans=cost1===cost2;
+
+//console.log(Ans);
+
+
+
+
+//var cost1=50;
+//var cost2=50;
+
+//var Ans=cost1===cost2;
+
+//console.log(Ans);
+
+
+//var cost1=39;
+
+//cost1 += 45;
+
+//console.log(cost1);
+
+
+//var shart1=67;
+//var shart2=80;
+
+//var total=shart1 != shart2;
+
+//console.log(total);
+
+
+
+
+
+
+//var shart1=80;
+//var shart2=80;
+
+//var total=shart1 != shart2;
+
+//console.log(total);
+
+
+//var shart1=67;
+//var shart2=80;
+
+//var total=shart1 !== shart2;
+
+//console.log(total);
+
+
+
+//var shart1=80;
+//var shart2="80";
+
+//var total=shart1 !== shart2;
+
+//console.log(total);
+
+
+//var shart1="67";
+//var shart2=67;
+
+//var total=shart1 !== shart2;
+
+//console.log(total);
+
+
+//var shart1="67";
+//var shart2="80";
+
+//var total=shart1 !== shart2;
+
+//console.log(total);
+
+
+var shart1="30";
+var shart2="30";
+
+var total=shart1 !== shart2;
+
+console.log(total);
+
+
+
+
+
+//var shart1=90;
+//var shart2=80;
+
+//var total=shart1 > shart2;
+
+//console.log(total);
+
+
+//var shart1=90;
+//var shart2=90;
+
+//var total=shart1 >= shart2;
+
+//console.log(total);
+
+//var shart1=89;
+//var shart2=90;
+
+//var total=shart1 >= shart2;
+
+//console.log(total);
+
+
+
+//var t=7;
+//var r=5;
+
+//var x=8;
+//var y=10;
+
+//var total= t > r && x < y;
+
+//console.log(total);
+
+
+
+//var t=7;
+//var r=5;
+
+//var x=8;
+//var y=5;
+
+//var total= t > r && x < y;
+
+//console.log(total);
+
+
+//var t=7;
+//var r=5;
+
+//var x=8;
+//var y=3;
+
+//var total= t > r || x < y;
+
+//console.log(total);
+
+
+
+
+//var shart1=90;
+//var shart2=120;
+
+//var total=shart1 < shart2;
+
+//console.log(total);
+
+
+
+//var shart1=70;
+//var shart2=80;
+
+//var total=shart1 < shart2;
+
+//console.log(total);
+
+
+
+//var shart1=70;
+//var shart2=50;
+
+//var total=shart1 < shart2;
+
+//console.log(total);
+
+
+
+
+
+
+
+
+
+//var cost1=39;
+
+//cost1 -= 45;
+
+//console.log(cost1);
+
+
+
+
+
+//var cost=500;
+//var product="gold";
+
+//console.log(typeof product);
+
+
+//var cost=700;
+//var product='gold';
+
+//console.log(typeof cost);
+
+
+
+//var cost=35000;
+//var compouter="IS"
+//var computerIstext=true;
+ 
+//console.log(typeof cost);
+
+
+//var cost=35000;
+//var product="IS"
+//var computerIstext=true;
+ 
+//console.log(typeof computerIstext);
+
+
+
+//var cost;
+//var product="cosmatic";
+
+//cost=500;
+
+//console.log(cost);
+
+
+//var tk1=400;
+//var tk2="60";
+
+//console.log(tk1+tk2);
+
+
+//var tk1=50;
+//var tk2=80;
+ //newTk=parseFloat(tk2);
+
+ //console.log(tk1+newTk);
+
+
+
+ //var cost1=30;
+ //var cost2="40";
+
+ //cost2=parseFloat(cost2);
+
+ //console.log(cost1+cost2);
+
+
+ //var price1=300;
+ //var price2="30.70";
+
+ //price2=parseFloat(price2);
+
+ //console.log(price1+price2);
+
+
+
+ //var price1=300;
+ //var price2="30.70";
+
+ //price2=parseInt(price2);
+
+ //var total=price1+price2;
+
+
+ //total=parseInt(total);
+
+ //console.log(total);
+
+
+
+ //var cost1=4.6;
+ //var cost2=3.1;
+
+ //var total=cost1+cost2;
+
+ //console.log(total);
+
+
+
+//var price1=90;
+//var price2=30;
+
+//price2=+price2;
+
+//var total=price1+price2;
+
+//console.log(total);
+
+
+
+//var cost2=400;
+//var cost4=300;
+
+//console.log(cost2+cost4);
+
+
+
+//var cost3=500;
+//var cost7=200;
+
+//console.log(cost3-cost7);
+
+
+
+
+//var price1=50;
+
+//var price2=30;
+
+//console.log(price1/price2);
+
+
+
+
+//var price5=70;
+//var price7=20;
+
+//console.log(price5-price7);
+
+//var pricex = 5;
+//var pricey = 2;
+//console.log(pricex * pricey);
+
+
+
+
+//var stockx = 10;
+//stockx += 5;
+
+//console.log(stockx);
+
+
+
+//var price=300;
+//var price=30;
+
+//console.log(price);
+
+
+
+//var price2=400;
+//var price4=300;
+
+//console.log(400 - 300);
+
+//console.log(50-30);
+
+
+
+
+
+//var price=600;
+
+//console.log(price);
+
+
+
+//var cost=300;
+//var cost=900;
+//console.log(cost);
+
+
+
+//var price=800;
+//var product="cosmatic";
+
+//console.log(price);
+
+
+
+//var cost=90;
+//var Product="tie";
+
+//console.log(cost);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
